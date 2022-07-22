8ddd8c864b3a74fa1fa5f9119f64e053cd0340fc
Brylimo <icj0103@gmail.com>
2022-07-22 14:18:18 +0900

    Add DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+	new ValidationPipe({
+		whitelist: true,
+		forbidNonWhitelisted: true, // 누군가 이상한거 보내면 req 자체를 막아버림
+		transform: true,
+	}));
   await app.listen(3000);
 }
 bootstrap();
