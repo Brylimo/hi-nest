5ee14efb749255da8466813c7134b76d7ce6a727
Brylimo <icj0103@gmail.com>
2022-07-22 14:47:41 +0900

    Add UpdateMovieDTO

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Post, Delete, Put, Patch, Body, Query } from '@
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -34,7 +35,7 @@ export class MoviesController {
 	}
 
 	@Patch("/:id")
-	patch(@Param('id') movieId: number, @Body() updateData){
+	patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDTO){
 		return this.moviesService.update(movieId, updateData);
 	} 
 }
